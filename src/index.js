498fc490689551b0c9361edfd4d194033a388f9d
진희정 <citron@mz.co.kr>
2022-10-20 06:20:45 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -11,11 +11,33 @@ const number = document.querySelector("span");
 // **스토어에 리듀서 담기**
 // const store = createStore(reducer);
 
-const countModifier = (state = 0) => {
+const countModifier = (state = 0, action) => {
   //유일하게 data를 바꿀 수 있는 곳.
-  console.log(state);
-  return state;
+  //이 곳에서 return하는 모든 것은 data가 된다.
+  //default로 data modifier(현재 코드의 countModifier)는 현재의 state와 함께 불려진다. (현재 코드의 state = 0)
+
+  //Action : redux에서 function을 부를 때 쓰는 두 번째 parameter 혹은 argument으로 reducer와 소통하기 위한 방법
+
+  console.log(state, action);
+
+  if (action.type === "ADD") {
+    return state + 1;
+  } else if (action.type === "MINUS") {
+    return state - 1;
+  } else {
+    return state;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+//Reducer에게 Action을 보내는 방법 : store.dispatch({key: value});
+countStore.dispatch({ type: "ADD" });
+//dispatch와 함께 countModifier로 메세지를 보낸다.
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
 console.log(countStore.getState());
