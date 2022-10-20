fd258ce6989022e1cb91d350ba46358137fcdc01
진희정 <citron@mz.co.kr>
2022-10-21 07:00:41 +0900

    State Mutation

--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
-const reducer = (state = [], action) => {
-  console.log(action);
+// state는 single source of truth이며, read-only이다
+// store을 수정할 수 있는 유일한 방법은 action을 보내는 방법뿐이다.
+// state를 mutate하지 말아야한다.
+// mutating state하는 대신에 new state objects를 리턴해야 한다.
+// (새로운 state를 create하고 그 새로운 state를 return 하는 걸 잊지 말아야 한다.)
 
+// ...state ▷ state array안의 모든 content
+
+const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text, id: action.id }];
     case DELETE_TODO:
       return [];
     default:
@@ -22,11 +28,17 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()));
+
 const onSubmit = (e) => {
   e.preventDefault();
+
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+
+  const toDate = Date.now();
+
+  store.dispatch({ type: ADD_TODO, text: toDo, id: toDate });
 };
 
 form.addEventListener("submit", onSubmit);
